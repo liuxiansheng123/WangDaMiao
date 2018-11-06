4521aef80691fa0ebac7307a827238b092541d8d
强 <15811442120@163.com>
2018-11-06 10:49:22 +0800

    mvvm的学习

---
+++ MyWfpDome/ViewModel/StudentViewModel.cs
@@ -0,0 +1,34 @@
+﻿using MyWfpDome.Model;
+using Prism.Commands;
+using Prism.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MyWfpDome.ViewModel
+{
+    public class StudentViewModel
+    {
+        public ICommand ButtonCommand
+        {
+            get
+            {
+                return new DelegateCommand<string>((str) =>
+                {
+                    ConvertText(str);
+                });
+            }
+        }
+
+        private void ConvertText(string str)
+        {
+            MessageBox.Show(str);
+        }
+    }
+}
