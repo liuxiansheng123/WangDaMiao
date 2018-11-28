18549f4b9d33ed090db74e6f4507fccad09e3bf9
强 <15811442120@163.com>
2018-11-28 15:50:49 +0800

    修改请求方式

--- WangDaMiao/WebApiHelp.cs
+++ WangDaMiao/WebApiHelp.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,47 +13,136 @@ namespace WangDaMiao
 {
     public class WebApiHelp
     {
-        private string HttpPost(string Url, string postDataStr)
+        static CookieContainer cookie = new CookieContainer();
+        //回调验证证书问题
+        private bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
         {
-            CookieContainer cookie = new CookieContainer();
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
-            request.Method = "POST";
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.ContentLength = Encoding.UTF8.GetByteCount(postDataStr);
-            request.CookieContainer = cookie;
-            Stream myRequestStream = request.GetRequestStream();
-            StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding("gb2312"));
-            myStreamWriter.Write(postDataStr);
-            myStreamWriter.Close();
-
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-            response.Cookies = cookie.GetCookies(response.ResponseUri);
-            Stream myResponseStream = response.GetResponseStream();
-            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
-            string retString = myStreamReader.ReadToEnd();
-            myStreamReader.Close();
-            myResponseStream.Close();
-
-            return retString;
+            // 总是接受    
+            return true;
         }
 
-        public string HttpGet(string Url, string postDataStr)
+        /// <summary>
+        /// get请求（有证书验证）
+        /// </summary>
+        /// <param name="Url">URL</param>
+        /// <returns></returns>
+        private string GetValidhtmlByGet(string Url)
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url + (postDataStr == "" ? "" : "?") + postDataStr);
-            request.Method = "GET";
-            request.ContentType = "text/html;charset=UTF-8";
+            HttpWebRequest webRequest;
+            HttpWebResponse webResponse;
+            try
+            {
+                //这一句一定要写在创建连接的前面。使用回调的方法进行证书验证。
+                ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(CheckValidationResult);
+                ServicePointManager.CheckCertificateRevocationList = true;
 
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Stream myResponseStream = response.GetResponseStream();
-            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
-            string retString = myStreamReader.ReadToEnd();
-            myStreamReader.Close();
-            myResponseStream.Close();
+                webRequest = (HttpWebRequest)WebRequest.Create(Url);
+                webRequest.Method = "GET";
+                webRequest.Accept = "*/*";
 
-            return retString;
+                // 获取对应HTTP请求的响应
+                webResponse = (HttpWebResponse)webRequest.GetResponse();
+                // 获取响应流
+                Stream responseStream = webResponse.GetResponseStream();
+                // 对接响应流(以"utf-8"字符集)
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                string result = reader.ReadToEnd();
+                reader.Close();
+                return result;
+
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+
+
         }
 
+        /// <summary>
+        /// 获取文件流
+        /// </summary>
+        /// <param name="Url"></param>
+        /// <returns></returns>
+        private Stream GetStreamByGet(string Url)
+        {
+            HttpWebRequest webRequest;
+            HttpWebResponse webResponse;
+            cookie = new CookieContainer();
+            try
+            {
+                //这一句一定要写在创建连接的前面。使用回调的方法进行证书验证。
+                ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(CheckValidationResult);
+                ServicePointManager.CheckCertificateRevocationList = true;
 
+                webRequest = (HttpWebRequest)WebRequest.Create(Url);
+                webRequest.Method = "GET";
+                webRequest.Accept = "*/*";
+                webRequest.CookieContainer = cookie;
+                // 获取对应HTTP请求的响应
+                webResponse = (HttpWebResponse)webRequest.GetResponse();
+
+                // 获取响应流
+                Stream responseStream = webResponse.GetResponseStream();
+                return responseStream;
+
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+
+        }
+
+        ///<summary>
+        ///post请求（有证书验证）
+        ///</summary>
+        ///<param name="URL">url地址</param>
+        ///<param name="strPostdata">发送的数据</param>
+        ///<returns></returns>
+        public string GetValidhtmlByPost(string url, string strPostData)
+        {
+            HttpWebRequest webRequest;
+            HttpWebResponse webResponse;
+            try
+            {
+                // 这一句一定要写在创建连接的前面。使用回调的方法进行证书验证。
+                ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(CheckValidationResult);
+                ServicePointManager.CheckCertificateRevocationList = true;
+
+                webRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+
+                webRequest.Method = "POST";
+                webRequest.ContentType = "application/x-www-form-urlencoded";
+                webRequest.KeepAlive = true;
+                webRequest.CookieContainer = cookie;
+
+
+                byte[] buffer = Encoding.UTF8.GetBytes(strPostData);
+                webRequest.ContentLength = buffer.Length;
+                webRequest.GetRequestStream().Write(buffer, 0, buffer.Length);
+
+                webResponse = (HttpWebResponse)webRequest.GetResponse();
+                StreamReader reader = new StreamReader(webResponse.GetResponseStream(), Encoding.UTF8);
+                string result = reader.ReadToEnd();
+                return result;
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message;
+
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
     }
 }
